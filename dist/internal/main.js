15caa0be883ab2379433ab7345d198d39bb8597b
sourcevault <joykrishnamondal@gmail.com>
2023-03-23 16:27:26 +0400

    rm known

--- dist/internal/main.js
+++ dist/internal/main.js
@@ -8,7 +8,7 @@ alpha_sort = vendor.alpha_sort;
 R = require('ramda');
 esp = require('error-stack-parser');
 _jspc = vendor.stringify;
-version = '2.0.2';
+version = '2.0.3';
 if (typeof window === "undefined" && typeof module === "object") {
   util = require('util');
   util_inspect_custom = util.inspect.custom;
